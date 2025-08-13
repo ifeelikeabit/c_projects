bf0f77c1a454165a9712fc99412e70f1823bdd06
ibrahim <ifeelikeabit@gmail.com>
2025-08-13 19:19:21 +0300

    init

---
+++ c_data_structers/linked_lists/singly_linked_list.c
@@ -0,0 +1,158 @@
+/*
+SINGLY LINKED LIST 
+! Sentinel value = NULL
+! value değeri gösterim amaçlıdır gerçek senaryolar için fonksiyonları mutlaka revize etmelisiniz.
+
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+typedef struct n{
+	int value;
+	struct n * next;
+}node;
+
+
+
+
+node *create_node(int value){ //Başka bir node göstermeyen, sahipsiz bir boş node oluşturur.
+	node *n = (node*)malloc(sizeof(node));
+	if (!n) { fprintf(stderr, "Bellek yetersiz\n"); exit(1); };
+	n->value = value;
+	n->next = NULL;
+	return n;
+}
+
+
+
+void print_list(node *root){//Tüm listeyi root null olana kadar yazdırır.
+	if(root==NULL){
+		printf("NULL\n");
+		return;
+	}
+
+	while(root!=NULL)
+	{
+		printf("%d->",root->value);
+		root = root->next;
+	}
+	printf("NULL\n");
+}
+
+
+void link(node *root, node* n){ //Herhangi bir node'u istenilen root'un sonuna bağlar.
+	if(root==NULL){perror("Root bir deger ifade etmiyor"); return;} 
+	if(n==NULL) {perror("Eklenecek node bir deger ifade etmiyor"); return;}
+	while(root->next!=NULL){
+		root = root->next;
+	}
+	root->next = n;
+	n->next = NULL;
+}
+
+void wipe(node **root){ //Tüm listeyi baştan sona siler.
+	node *tmp;
+	while(*root!=NULL){
+
+		tmp = *root; 
+		*root = (*root)->next;
+		free(tmp);
+
+	}
+*root = NULL;
+}
+
+
+
+void delete_node(node **root , node *n){ //Node'un root olup olmama durumuna bakar ve node u siler.
+		
+	if(*root==NULL){
+		perror("Root bir deger ifade etmiyor\n");
+		return;
+	}
+	if(n==NULL){
+		perror("Silinecek node bir deger ifade etmiyor\n");
+		return;
+	}
+	if(*root==n){ //İlk değer silinecekse.
+		*root = (*root)->next;
+		free(n);
+		return;
+	}
+
+node *iter = *root;
+
+while (iter->next != NULL && iter->next != n)iter=iter->next;
+if(iter->next == NULL) {// Listenin sonua geldi ise;
+	perror("Silinecek node bulunamadi\n");
+	return;
+}
+iter->next = n->next;
+free(n);
+return;
+}
+
+
+
+node * add_new(node **root, int value){ //Rootun sonuna yeni bir node oluşturup bağlar. Eklenen node'u döndürür. 
+	node * n = create_node(value);
+	if(*root==NULL){
+		*root=n; 
+		return *root;
+
+	}
+	link(*root,n);
+	return n;
+}
+
+//SIRALI EKLEME KÜÇÜKTEN BÜYÜĞE
+node * add_new_sorted(node **root, int value){//Kucukten buyuge eleman ekler. Eklenen node'u döndürür. 
+	node * n = create_node(value);
+	if(*root==NULL) {//Root yoksa.
+		*root=n; 
+		return *root;
+	}
+	if((*root)->next == NULL && (*root)->value <= value){ //1 node varsa ve value roottan büyük ve eşitse.
+		(*root)->next = n;
+		return n;
+	}
+	if((*root)->value >= value) { //Eklenen değer en küçükse ve eşitse.
+		n->next = *root;
+		*root = n;
+		return n;
+	}
+	node *iter = *root;
+
+	while(iter->next != NULL && iter->next->value <= value)
+		iter=iter->next;
+	if(iter->next == NULL) //Son değere ulaştıysa.
+	{
+		iter->next= n;
+		return n;
+	}
+	n->next = iter->next; //Son değere ulaşmadan durdu.
+	iter->next = n;
+
+
+	return n;
+
+}
+
+
+
+
+int main(){
+	//örnek
+	node * root = create_node(0);
+	for(int i=0;i<10;i++){
+		add_new_sorted(&root,i*10);
+	}
+	print_list(root);
+
+	wipe(&root);
+}
