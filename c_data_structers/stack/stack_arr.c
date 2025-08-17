f56c381ee5e140ab8f6ae0d564aaa5c1d57a7ed3
ibrahim <ifeelikeabit@gmail.com>
2025-08-18 00:37:14 +0300

    added null controls

--- c_data_structers/stack/stack_arr.c
+++ c_data_structers/stack/stack_arr.c
@@ -3,38 +3,39 @@
 #include "stack_arr.h"
 
 stack* init_stack(){
-
-stack *s = (stack*)malloc(sizeof(stack));
-s->array = (int*)malloc(sizeof(int));
-s->top = -1;
-s->capacity = 2; 
-fprintf(stdout,"stack initilazed with;\ncapacity %d\ntop %d\n",s->capacity,s->top);
-
-return s;}
+	stack *s = (stack*)malloc(sizeof(stack));
+	s->array = (int*)malloc(sizeof(int));
+	s->top = -1;
+	s->capacity = 2; 
+	stack_info(s,0);
+	return s;
+}
 
 void push(stack* x,int value){
-
-if(x->capacity==x->top+1){//when the capacity reach full.
-	fprintf(stdout,"capacity is full: %d\n",x->capacity);
-	int *new = (int*)malloc(sizeof(int)*2*(x->capacity));
-	for(int i=0; i<=x->top; i++)
-		new[i]=x->array[i];
-	free(x->array);
-	x->array = new; //Transfering new array adress from new array pointer.
-	x->capacity *=2;
-	fprintf(stdout,"capacity increased to: %d\n",x->capacity);
-}
-x->top++;
-x->array[x->top] = value;
-fprintf(stdout,"pushed value: %d\n",value);
+	if(!x->array){  //If array not initilazed.
+		x->array  = (int*)malloc(sizeof(int)*2);
+	}
+	if(x->capacity==x->top+1){//when the capacity reach full.
+		fprintf(stdout,"capacity is full: %d\n",x->capacity);
+		int *new = (int*)malloc(sizeof(int)*2*(x->capacity));
+		for(int i=0; i<=x->top; i++)
+			new[i]=x->array[i];
+		free(x->array);
+		x->array = new; //Transfering new array adress from new array pointer.
+		x->capacity *=2;
+		fprintf(stdout,"capacity increased to: %d\n",x->capacity);
+	}
+	x->top++;
+	x->array[x->top] = value;
+	fprintf(stdout,"pushed value: %d\n",value);
 }
 
 void pop(stack* x){
-	if(x->top<0){
-	fprintf(stderr,"Pop error: this stack not have a value!\n");
-	return;
+	if(x->top<0 || !x->array){
+		fprintf(stderr,"Pop error: this stack not have a value!\n");
+		return;
 	}
-	if(x->top+1 <= x->capacity/4){ //when the elements less than capacity / 4.
+	if(x->top+1 <= x->capacity/4 && x->capacity > 2){ //when the elements less than capacity / 4.
 		fprintf(stdout,"capacity is too large for it haved values: %d\n",x->capacity);
 		int *new = (int*)malloc(sizeof(int)*(x->capacity)/2);
 		for(int i=0; i<=x->top; i++)
