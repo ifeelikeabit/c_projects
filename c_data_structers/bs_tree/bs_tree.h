dfb0ee930a045cb893fab57f865701859c9ecb56
ibrahim <ifeelikeabit@gmail.com>
2025-08-22 11:40:36 +0300

    changed directory name:bs_tree

---
+++ c_data_structers/bs_tree/bs_tree.h
@@ -0,0 +1,31 @@
+#ifndef bs_tree_h
+#define bs_tree_h
+//If the next pointer of any node is null then we call that node as leaf(Execpt root). 
+
+typedef struct node {
+int value;
+struct node* right;
+struct node* left;
+
+}node;
+
+node *create_node(int);
+
+node* search_item(node*,int); //Search item by value. Finded: 1 .Nothing: 0, Error: -1.
+
+int del_item(node **, int); //Delete searched value and fixs tree shifts if we need.
+
+int add_item(node**, int); 
+
+
+
+
+
+//We can easly make a switch case but i want to show different algorithms with specific name.
+void print_rnl(node*); // Prints tree from big to small (Right Node Left)
+void print_lnr(node*); // Prints tree from small to big (Right Node Left)
+void print_nrl(node*);
+
+
+
+#endif
---
+++ c_data_structers/bs_tree/test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "bs_tree.h"
+
+
+int main(){
+
+
+
+node * root = create_node(100);
+
+
+for (int i=0; i<=10; i++) {
+printf("%dAdd_item %d\n",i*10,add_item(&root, i*10));
+}
+
+
+
+}
